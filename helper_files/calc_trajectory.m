6ccb7e9767d1d02605b39fefc22f930f0295f2ea
Ani Gokhale <anigokhale321@gmail.com>
2023-01-30 19:15:09 -0500

    Added a bunch of files

--- helper_files/calc_trajectory.m
+++ helper_files/calc_trajectory.m
@@ -1,52 +1,18 @@
-function [traj, t] = calc_trajectory(xInitial, xFinal, xWaypoints, numPoints, endTime)
+function traj = calc_trajectory(timeVector, xInit, linAcc, angAcc)
+options = odeset('RelTol',1e-8,'AbsTol',1e-10);
+[timeVector, x] = ode45(@(t,x) fun(t, x, linAcc, angAcc), [timeVector(1), timeVector(end)], xInit, options);
+fplot(angAcc(2), [timeVector(1) timeVector(end)]);
+figure;
+plot(timeVector, x(:, 5));
 
-posInit = xInitial(1:3);
-posFinal = xFinal(1:3);
-if ~isempty(xWaypoints)
-    posWaypoints = xWaypoints(1:3, :);
-else 
-    posWaypoints = [];
+traj = x;
+
 end
 
-posTraj = [posInit, posWaypoints, posFinal];
-
-posTrajNew = interparc(numPoints, posTraj(1, :), posTraj(2, :), posTraj(3, :), 'spline')';
-X1 = posTrajNew(1, :);
-X2 = posTrajNew(2, :);
-X3 = posTrajNew(3, :);
-t = linspace(0, endTime, numPoints);
-VX1 = gradient(X1, t);
-VX2 = gradient(X2, t);
-VX3 = gradient(X3, t);
-
-subplot(2, 2, 1);
-plot3(posTraj(1, :), posTraj(2, :), posTraj(3, :), 'r.', X1, X2, X3, 'b-');
-subplot(2, 2, 2);
-plot3(VX1, VX2, VX3, 'g-');
-
-oriInit = xInitial(4:6);
-oriFinal = xFinal(4:6);
-if ~isempty(xWaypoints)
-    oriWaypoints = xWaypoints(1:3, :);
-else 
-    oriWaypoints = [];
+function dxdt = fun(t, x, linAcc, angAcc)
+    dxdt(1:6) = x(7:12)';
+    linAccEval = subs(linAcc, t);
+    angAccEval = subs(angAcc, t);
+    dxdt(7:12) = [linAccEval; angAccEval];
+    dxdt = dxdt';
 end
-
-oriTraj = [oriInit, oriWaypoints, oriFinal];
-
-oriTrajNew = interparc(numPoints, oriTraj(1, :), oriTraj(2, :), oriTraj(3, :), 'spline')';
-phi = oriTrajNew(1, :);
-theta = oriTrajNew(2, :);
-psi = oriTrajNew(3, :);
-omega_phi = gradient(phi, t);
-omega_theta = gradient(theta, t);
-omega_psi = gradient(psi, t);
-
-subplot(2, 2, 3);
-plot3(oriTraj(1, :), oriTraj(2, :), oriTraj(3, :), 'r.', phi, theta, psi, 'b-');
-subplot(2, 2, 4);
-plot3(omega_phi, omega_theta, omega_psi, 'g-');
-
-traj = [X1; X2; X3; phi; theta; psi; VX1; VX2; VX3; omega_phi; omega_theta; omega_psi];
-end
-
--- helper_files/fourier_series.m
+++ helper_files/fourier_series.m
@@ -1,12 +1,12 @@
-function [F_t, L] = thrust_series(coarse_t, coarse_thrust, num_coefficients)
+function [F_t, L] = fourier_series(coarse_t, coarse_signal, num_coefficients, num_points)
 
-time = linspace(coarse_t(1), coarse_t(end), 1000);
-thrust = interp1(coarse_t, coarse_thrust, time);
+time = linspace(coarse_t(1), coarse_t(end), num_points);
+signal = interp1(coarse_t, coarse_signal, time);
 
 L = time(end);
 numCoeff = num_coefficients;
 
-A0 = (1/2/L) * trapz(time, thrust);
+A0 = (1/2/L) * trapz(time, signal);
 
 An = zeros(1, numCoeff);
 Bn = zeros(1, numCoeff);
@@ -18,8 +18,8 @@ for n = 1:numCoeff
     weightedCos = zeros(1, length(time));
     weightedSin = zeros(1, length(time));
     for m = 1:length(time)
-        weightedCos(m) = thrust(m) * cos(n * pi * time(m) / L);
-        weightedSin(m) = thrust(m) * sin(n * pi * time(m) / L);
+        weightedCos(m) = signal(m) * cos(n * pi * time(m) / L);
+        weightedSin(m) = signal(m) * sin(n * pi * time(m) / L);
     end
     An(n) = trapz(time, weightedCos) / L;
     Bn(n) = trapz(time, weightedSin) / L;
