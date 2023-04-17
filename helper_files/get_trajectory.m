6e1e19c1bd60550b7d21df6ec5fcac1bc2e2ccfd
Ani Gokhale <anigokhale321@gmail.com>
2023-04-17 19:59:09 -0400

    Added continuity protection

--- helper_files/get_trajectory.m
+++ helper_files/get_trajectory.m
@@ -1,11 +1,11 @@
-function [xset, uset, tset, Kset, tSegs] = get_trajectory(numPoints, xcritset, ucritset, constants, MOI)
+function [xset, uset, tset, Kset, tSegs] = get_trajectory(numPoints, tf, xcritset, ucritset, constants, MOI)
     mass = constants(1);
     grav = constants(3);
     numCrits = size(xcritset, 2);
     tset = zeros(1, numCrits*numPoints);
     
     ti = 0;
-    tf = 5;
+%     tf = 7.5;
     tSegs = linspace(ti, tf, numCrits);
 
     rmax = [1; 1; 1];
@@ -26,7 +26,11 @@ function [xset, uset, tset, Kset, tSegs] = get_trajectory(numPoints, xcritset, u
     Kset = zeros(size(uset, 1), size(xset, 1)+3, numCrits);
 
     for i = 1:(numCrits - 1)
-        [xset(:, (1+numPoints*(i-1)):numPoints*i), uset(:, (1+numPoints*(i-1)):numPoints*i), tset(1, (1+numPoints*(i-1)):numPoints*i), Kset(:, :, i)] = get_segment_traj(numPoints, tSegs(i), tSegs(i+1), xcritset(:, i), xcritset(:, i+1), ucritset(:, i+1), Q, R, constants, MOI);
+        if i > 1
+            [xset(:, (1+numPoints*(i-1)):numPoints*i), uset(:, (1+numPoints*(i-1)):numPoints*i), tset(1, (1+numPoints*(i-1)):numPoints*i), Kset(:, :, i)] = get_segment_traj(numPoints, tSegs(i), tSegs(i+1), xset(:, numPoints*(i-1)), xcritset(:, i+1), ucritset(:, i+1), Q, R, constants, MOI);
+        else
+            [xset(:, (1+numPoints*(i-1)):numPoints*i), uset(:, (1+numPoints*(i-1)):numPoints*i), tset(1, (1+numPoints*(i-1)):numPoints*i), Kset(:, :, i)] = get_segment_traj(numPoints, tSegs(i), tSegs(i+1), xcritset(:, i), xcritset(:, i+1), ucritset(:, i+1), Q, R, constants, MOI);
+        end
         xset(:, (1+numPoints*(i-1)):numPoints*i) = xset(:, (1+numPoints*(i-1)):numPoints*i) + xcritset(:, i+1);
     end
 end
