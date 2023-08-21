ee3e4737279bcf18d4c1fe7d464eff224f1a4200
Ani Gokhale <anigokhale321@gmail.com>
2023-08-21 19:13:26 -0400

    bugfixes

--- getParams.m
+++ getParams.m
@@ -26,21 +26,22 @@ q3 = q3(2:end);
 x3 = [1; 0; 0; 0; 0; 0; q3; 0; 0; 0];
 u3 = [0; 0; m*g; 0];
 
+%Maximum and minimum values for input for simulation
 inputLimits = [-pi/12, pi/12; -pi/12, pi/12; 0, 100; -2, 2];
+
+%Throttle -> Thrust force equation constants
 throttleConsts = [-0.000112; 0.02072; -.268];
 
+%Delay for servo angle inputs
 betaInputDelay = 0.001;
 gammaInputDelay = 0.001;
 
 %SIMULATE and create trajectory
 fprintf("Creating Trajectory\n");
-[x_set, u_set, t_set_unfiltered, Kset, tSegs] = get_trajectory(10000, 10, [x1, x2, x3], [u1, u2, u3], [m; l; g], MOI, inputLimits, throttleConsts);
-[t_set, ia, ~] = unique(t_set_unfiltered, 'stable');
-x_set = x_set(:, ia);
-u_set = u_set(:, ia);
+[x_set, u_set, t_set, Kset, tSegs, startTime, stopTime] = get_trajectory(10000, 10, [x1, x2, x3], [u1, u2, u3], [m; l; g], MOI, inputLimits, throttleConsts);
+
+%plot trajectory
 plotTrajectory(x_set, u_set, t_set, 0.75, 500, [m; l; g]);
-startTime = t_set(1);
-stopTime = t_set(end);
 
 fprintf("Done initializing!\n");
 cd ../
--- helper_files/get_trajectory.m
+++ helper_files/get_trajectory.m
@@ -1,11 +1,16 @@
-function [xset, uset, tset, Kset, tSegs] = get_trajectory(numPoints, tf, xcritset, ucritset, constants, MOI, limits, throttleConsts)
+function [x_set, u_set, t_set, K_set, tSegs, startTime, stopTime] = get_trajectory(numPoints, tf, xcritset, ucritset, constants, MOI, limits, throttleConsts)
+    %Number of critical points in trajectory
     numCrits = size(xcritset, 2);
-    tset = zeros(1, numCrits*numPoints);
+    %Full timeline initializations
+    t_set = zeros(1, numCrits*numPoints);
     
+    %initial time
     ti = 0;
-
+    
+    %time segment limits
     tSegs = linspace(ti, tf, numCrits);
 
+    %maximum values for state and input used for Bryson's rule
     rmax = [1; 1; 1];
     vmax = [.5; 1; 1];
     qmax = angle2quat(pi/12, pi/12, pi/12, 'ZYX')';
@@ -19,16 +24,25 @@ function [xset, uset, tset, Kset, tSegs] = get_trajectory(numPoints, tf, xcritse
     Q = diag(xmax.^-2);
     R = diag(umax.^-2);
 
+    %trajectory set initialization
     xset = zeros(size(xcritset, 1), numPoints*numCrits);
     uset = zeros(size(ucritset, 1), numPoints*numCrits);
-    Kset = zeros(size(uset, 1), size(xset, 1)+3, numCrits);
+    K_set = zeros(size(uset, 1), size(xset, 1)+3, numCrits);
 
+    %Create trajectories for different segments
     for i = 1:(numCrits - 1)
         if i > 1
-            [xset(:, (1+numPoints*(i-1)):numPoints*i), uset(:, (1+numPoints*(i-1)):numPoints*i), tset(1, (1+numPoints*(i-1)):numPoints*i), Kset(:, :, i)] = get_segment_traj(numPoints, tSegs(i), tSegs(i+1), xset(:, numPoints*(i-1)), xcritset(:, i+1), ucritset(:, i+1), Q, R, constants, MOI, limits, throttleConsts);
+            [xset(:, (1+numPoints*(i-1)):numPoints*i), uset(:, (1+numPoints*(i-1)):numPoints*i), t_set(1, (1+numPoints*(i-1)):numPoints*i), K_set(:, :, i)] = get_segment_traj(numPoints, tSegs(i), tSegs(i+1), xset(:, numPoints*(i-1)), xcritset(:, i+1), ucritset(:, i+1), Q, R, constants, MOI, limits, throttleConsts);
         else
-            [xset(:, (1+numPoints*(i-1)):numPoints*i), uset(:, (1+numPoints*(i-1)):numPoints*i), tset(1, (1+numPoints*(i-1)):numPoints*i), Kset(:, :, i)] = get_segment_traj(numPoints, tSegs(i), tSegs(i+1), xcritset(:, i), xcritset(:, i+1), ucritset(:, i+1), Q, R, constants, MOI, limits, throttleConsts);
+            [xset(:, (1+numPoints*(i-1)):numPoints*i), uset(:, (1+numPoints*(i-1)):numPoints*i), t_set(1, (1+numPoints*(i-1)):numPoints*i), K_set(:, :, i)] = get_segment_traj(numPoints, tSegs(i), tSegs(i+1), xcritset(:, i), xcritset(:, i+1), ucritset(:, i+1), Q, R, constants, MOI, limits, throttleConsts);
         end
         xset(:, (1+numPoints*(i-1)):numPoints*i) = xset(:, (1+numPoints*(i-1)):numPoints*i) + xcritset(:, i+1);
     end
+    
+    %Get rid of duplicate points in trajectory
+    [t_set, ia, ~] = unique(t_set, 'stable');
+    x_set = xset(:, ia);
+    u_set = uset(:, ia);
+    startTime = t_set(1);
+    stopTime = t_set(end);
 end
