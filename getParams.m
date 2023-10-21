ec7863bdcbb9734d72e27924a41062d341ceb788
Ani Gokhale <anigokhale321@gmail.com>
2023-10-21 11:47:21 -0400

    Reorganization and refactoring

--- getParams.m
+++ getParams.m
@@ -1,17 +1,15 @@
 clear;
 %Get constants from PDM integration
-cd ./PDM_integration/
+addpath('lib\');
+addpath('PDM_integration\');
 raw = readmatrix("/parameters.xlsx");
-cd ../
 MOI = raw(1:3, 2:4);
 l = raw(4, 2);
 m = raw(5, 2);
 g = 9.81;
 
-cd ./helper_files/
-
 %Enumerate operating points for trajectory below:
-initEul = [pi/2, pi/6, pi/6];
+initEul = [pi/2, pi/6, pi/3];
 q1 = angle2quat(initEul(1), initEul(2), initEul(3), 'XYZ')';
 q1 = q1(2:end);
 x1 = [10; 10; 10; 0; 0; 0; q1; 0; 0; 0];
@@ -19,7 +17,7 @@ u1 = [0; 0; m*g; 0];
 
 q2 = angle2quat(0, 0, 0, 'XYZ')';
 q2 = q2(2:end);
-x2 = [5; 5; 0; 0; 0; 0; q2; 0; 0; 0];
+x2 = [5; 5; 5; 0; 0; 0; q2; 0; 0; 0];
 u2 = [0; 0; m*g; 0];
 
 q3 = angle2quat(0, 0, 0, 'XYZ')';
@@ -42,7 +40,6 @@ fprintf("Creating Trajectory\n");
 [x_set, u_set, t_set, Kset, tSegs, startTime, stopTime] = get_trajectory(10000, 27, [x1, x2, x3], [u1, u2, u3], [m; l; g], MOI, inputLimits, throttleConsts);
 
 %plot trajectory
-plotTrajectory(x_set, u_set, t_set, 0.75, 50, [m; l; g]);
+plotTrajectory(x_set, u_set, t_set, 0.75, 500, [m; l; g]);
 
 fprintf("Done initializing!\n");
-cd ../
